d5de03d3ec3e591724f3ed3a6d785dc728046aa9
Kelly Byrne <kellyannbyrne@utexas.edu>
2015-11-12 16:18:25 -0700

    getting there

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.post('/api/v1/memories', function(req, res, next) {
   pg.connect(conString, function(err, client, done) {
   	client.query('INSERT INTO memories (old_days, these_days, year) VALUES ($1, $2, $3)', [req.body.data.attributes.old_days, req.body.data.attributes.these_days, req.body.data.attributes.year], function(err, result) {
     	done();
-    	res.json(result);
+    	res.sendStatus(200);
   	});
 	});
 });
@@ -25,6 +25,18 @@ router.get('/api/v1/memories', function(req, res, next) {
 	});
 });
 
+router.get('/api/v1/memories/years', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+  	client.query('SELECT DISTINCT year FROM memories', function(err, result) {
+    	done();
+    	var output = [];
+    	for(var i=0; i<result.rows.length; i++) {
+    		output.push(result.rows[i].year)
+    	}
+    	res.json(output);
+  	});
+	});
+});
 
 router.get('/api/v1/memories/:year', function(req, res, next) {
   pg.connect(conString, function(err, client, done) {
@@ -36,13 +48,6 @@ router.get('/api/v1/memories/:year', function(req, res, next) {
 });
 
 
-// router.get('/api/v1/memories/years', function(req, res, next) {
-//   pg.connect(conString, function(err, client, done) {
-//   	client.query('SELECT year FROM memories', function(err, result) {
-//     	done();
-//     	res.json(result);
-//   	});
-// 	});
-// });
+
 
 module.exports = router;
